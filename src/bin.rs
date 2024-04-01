296611e5be4f06639b3b4c1e3a7440bc0806aa4b
Santoso Wijaya <code@swijaya.me>
2024-03-31 21:08:57 -0700

    Define `colorschemes::all()` that returns all palettes

--- src/bin.rs
+++ src/bin.rs
@@ -1,11 +1,11 @@
-use base16cs::colorschemes::{selenized, solarized};
+use base16cs::colorschemes;
 use base16cs::palette;
 
 fn main() {
-    print_palette(&solarized::SOLARIZED_DARK);
-    print_palette(&solarized::SOLARIZED_LIGHT);
-    print_palette(&selenized::SELENIZED_DARK);
-    print_palette(&selenized::SELENIZED_LIGHT);
+    for (name, palette) in colorschemes::all().into_iter() {
+        print!("\"{name}\": ");
+        print_palette(palette);
+    }
 }
 
 fn print_palette(palette: &palette::Palette) {
--- src/colorschemes/mod.rs
+++ src/colorschemes/mod.rs
@@ -1,2 +1,22 @@
 pub mod selenized;
 pub mod solarized;
+
+use crate::palette::Palette;
+use std::collections::HashMap;
+
+/// return all colorschemes keyed by their names
+pub fn all<'a>() -> HashMap<&'a str, &'a Palette> {
+    let all_colorschemes = vec![
+        &solarized::SOLARIZED_DARK,
+        &solarized::SOLARIZED_LIGHT,
+        &selenized::SELENIZED_DARK,
+        &selenized::SELENIZED_LIGHT,
+    ];
+
+    let mut m = HashMap::new();
+    for palette in all_colorschemes {
+        m.entry(palette.name).or_insert(palette);
+    }
+
+    m
+}
