4c96533237b4f86b5f644a3815e1c405d5986972
Santoso Wijaya <code@swijaya.me>
2024-05-12 00:24:29 -0700

    Top-level rustdoc and some doc tests
    gh #1

--- base16cs/src/lib.rs
+++ base16cs/src/lib.rs
@@ -1,3 +1,5 @@
+//! # `base16cs`
+//!
 //! A library for defining a **palette** of **base colors** in canonical CIE
 //! L\*a\*b\* colorspace values, and then deriving other colorspace values from
 //! them.
@@ -6,10 +8,12 @@
 //! a template renderer (Liquid, by default) for injecting palette and color
 //! variables into a template for renders.
 //!
-//! # Examples
+//! ## Examples
 //!
-//! ## Serde
+//! ```rust
+//! use base16cs::{Palette, BaseColor, DerivedPalette};
 //!
+//! // Define a canonical palette
 //! let palette = Palette::new(
 //!     "My Palette",
 //!     [
@@ -17,6 +21,47 @@
 //!         BaseColor::new("fg", 31, -6, -6),
 //!     ]);
 //!
+//! // Derive with computed sRGB values
+//! let derived_palette = DerivedPalette::from(&palette);
+//! assert_eq!(derived_palette.name, "My Palette");
+//!
+//! let derived_colors = &derived_palette.colors;
+//! assert_eq!(derived_colors[0].base, &palette.colors[0]);
+//! assert_eq!(derived_colors[1].base, &palette.colors[1]);
+//!
+//! assert_eq!(derived_colors[0].srgb_hex, "fef3da");
+//! assert_eq!(derived_colors[1].srgb_hex, "384c52");
+//!
+//! // Serialize (to YAML)
+//! use base16cs::Serializable;
+//!
+//! let serialized = derived_palette.serialize().unwrap();
+//! assert_eq!(serialized, r#"name: My Palette
+//! colors:
+//! - base:
+//!     name: bg
+//!     lab:
+//!       l: 96.0
+//!       a: 0.0
+//!       b: 13.0
+//!   srgb:
+//!     red: 254
+//!     green: 243
+//!     blue: 218
+//!   srgb_hex: fef3da
+//! - base:
+//!     name: fg
+//!     lab:
+//!       l: 31.0
+//!       a: -6.0
+//!       b: -6.0
+//!   srgb:
+//!     red: 56
+//!     green: 76
+//!     blue: 82
+//!   srgb_hex: 384c52
+//! "#);
+//! ```
 
 mod palette;
 mod serialize;
